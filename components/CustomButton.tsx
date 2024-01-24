12529da9498ff7162328a2cd70c239c54b48f48e
PaulStephane24 <paulstephanebomahi12@gmail.com>
2024-01-24 22:38:04 +0000

    end_first_session

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { CustomButtonProps } from '@/types';
+import Image from 'next/image';
 import React from 'react'
 
 const CustomButton = ({
     title,
     btnType,
     containerStyles,
+    textStyles,
+    rightIcon,
     handleClick
 } : CustomButtonProps) => {
   return (
@@ -17,10 +20,19 @@ const CustomButton = ({
         onClick={handleClick}
     >
 
-        <span className={`flex-1`}>
+        <span className={`flex-1 ${textStyles}`}>
             {title}
         </span>
-
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image 
+            src={rightIcon}
+            alt={'right icon'}
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   )
 }
---
+++ components/SearchBar.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { useState } from "react";
+import { SearchManufacturer } from ".";
+
+const SearchBar = () => {
+  const handleSearch = () => {}
+  const [manufacturer, setManufacturer] = useState('');
+
+
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManuFacturer={setManufacturer}
+        />
+      </div>
+    </form>
+  )
+}
+
+export default SearchBar
